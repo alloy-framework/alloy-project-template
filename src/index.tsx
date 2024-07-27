8a00e344e3409c661feb496daebca19a271c8e36
Brian Terlson <brian.terlson@microsoft.com>
2024-07-26 19:51:04 -0700

    Update deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { Output, render } from "@alloy-js/core";
+import { Output, refkey, render } from "@alloy-js/core";
 import * as ts from "@alloy-js/typescript";
 const output = render(
   <Output>
@@ -10,15 +10,15 @@ const output = render(
     </ts.SourceFile>
 
     <ts.SourceFile path="test2.ts">
-      <ts.FunctionDeclaration export default name="test1" refkey="test3" />
-      <ts.FunctionDeclaration export name="test2" refkey="test4" />
+      <ts.FunctionDeclaration export default name="test1" refkey={refkey("test3")} />
+      <ts.FunctionDeclaration export name="test2" refkey={refkey("test4")} />
     </ts.SourceFile>
 
     <ts.SourceFile path="test3.ts">
-      const v1 = <ts.Reference refkey="test1" />;
-      const v2 = <ts.Reference refkey="test2" />;
-      const v3 = <ts.Reference refkey="test3" />;
-      const v4 = <ts.Reference refkey="test4" />;
+      const v1 = <ts.Reference refkey={refkey("test1")} />;
+      const v2 = <ts.Reference refkey={refkey("test2")}/>;
+      const v3 = <ts.Reference refkey={refkey("test3")}/>;
+      const v4 = <ts.Reference refkey={refkey("test4")} />;
     </ts.SourceFile>
   </Output>
 );
